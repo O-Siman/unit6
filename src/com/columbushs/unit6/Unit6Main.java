f70ebe64c06df572f9344dc927dab97176945b58
O-Siman <72103629+O-Siman@users.noreply.github.com>
2021-11-02 11:12:41 -0400

    Initial

---
+++ src/com/columbushs/unit6/Unit6Main.java
@@ -0,0 +1,8 @@
+package com.columbushs.unit6;
+
+public class Unit6Main {
+    public static void main(String[] args) {
+        Unit6_1ArrayCreationAndAccess arrayTests = new Unit6_1ArrayCreationAndAccess();
+        arrayTests.arrayTesting();
+    }
+}
